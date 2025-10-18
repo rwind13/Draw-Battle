fd42bdf3d5ef6dc9d9baff1b19169dd1f6c32bbc
Ryan Windle <rwindle913@gmail.com>
2025-10-17 22:54:35 -0400

    Third time is the charm. App skeleton is set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5001");
+const socket = io("http://localhost:5000");
 
 function App() {
   const [prompt, setPrompt] = useState("");
@@ -13,7 +13,7 @@ function App() {
   }, []);
 
   const getPrompt = async () => {
-    const res = await fetch("http://localhost:5001/api/prompt");
+    const res = await fetch("http://localhost:5000/api/prompt");
     const data = await res.json();
     setPrompt(data.prompt);
   };
